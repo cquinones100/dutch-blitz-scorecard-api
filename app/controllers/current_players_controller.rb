0d60ccc59429c3fe6a287846d14de53d85655faf
Carlos Quinones <cquinones100@gmail.com>
2022-02-17 22:09:53 -0500

    add player serializer

--- app/controllers/current_players_controller.rb
+++ app/controllers/current_players_controller.rb
@@ -8,7 +8,7 @@ class CurrentPlayersController < ApplicationController
     )
 
     if player
-      render json: Serializer.serialize(player, :name, lobby_id: encode(player.lobby_id))
+      render json: player.serialize
     else
       render json: {}, status: 404
     end
